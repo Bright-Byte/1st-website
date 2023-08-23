0808591df1dd0f5791015df24206391f728eb6df
Bright-Byte <128817018+Bright-Byte@users.noreply.github.com>
2023-08-23 12:38:10 +1000

    Update videogame.js

--- Video game/videogame.js
+++ Video game/videogame.js
@@ -49,8 +49,9 @@ function preload() {
     this.load.image('bomb', 'images/bomb.png')
     this.load.image('ground', 'images/Floor.PNG')
     this.load.spritesheet('dude', 'images/george.png', {
-        frameWidth: 30,
-        frameHeight: 30
+        frameWidth: 48,
+        frameHeight: 48,
+        scale: 0.5
     });
     this.load.image('bground2', 'images/DC4F2704-263F-465C-BAB8-5C15682682D8.jpeg')
     this.load.audio('music', ['music/Y2Mate.is - Old RuneScape Soundtrack Sea Shanty2-BJhF0L7pfo8-48k-1654852079074.mp3'])
