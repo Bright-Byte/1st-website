cc91002c0c270beebcd2e7162fc715b2681d9e41
Bright-Byte <128817018+Bright-Byte@users.noreply.github.com>
2024-04-28 20:48:08 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const io = require('socket.io')(3000)
-
+const express = require('express'); //Line 1
+const app = express()
+const port = process.env.PORT || 5500;
 const users = {}
+const http = require('http').Server(app);
+const io = require('socket.io')(http)
+
+//server
+http.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
